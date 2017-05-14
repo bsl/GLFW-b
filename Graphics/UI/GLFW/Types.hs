78a5ac8575a11da08aa236da4092d0b7f891c2b1
Lokathor <zefria@gmail.com>
2017-05-13 18:46:28 -0600

    Added docs to the Types module

--- Graphics/UI/GLFW/Types.hs
+++ Graphics/UI/GLFW/Types.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
+-- | The types of the package. This module is considered "internal", and the
+-- types are re-exported from Graphics.UI.GLFW as necessary.
 module Graphics.UI.GLFW.Types where
 
 --------------------------------------------------------------------------------
@@ -19,21 +21,24 @@ import Bindings.GLFW
 --------------------------------------------------------------------------------
 -- Error handling
 
+-- | An enum for one of the <http://www.glfw.org/docs/3.1/group__errors.html#ga196e125ef261d94184e2b55c05762f14 GLFW error codes>.
 data Error =
-    Error'NotInitialized
-  | Error'NoCurrentContext
-  | Error'InvalidEnum
-  | Error'InvalidValue
-  | Error'OutOfMemory
-  | Error'ApiUnavailable
-  | Error'VersionUnavailable
-  | Error'PlatformError
-  | Error'FormatUnavailable
+    Error'NotInitialized -- ^ <http://www.glfw.org/docs/3.1/group__errors.html#ga2374ee02c177f12e1fa76ff3ed15e14a doc>
+  | Error'NoCurrentContext -- ^ <http://www.glfw.org/docs/3.1/group__errors.html#gaa8290386e9528ccb9e42a3a4e16fc0d0 doc>
+  | Error'InvalidEnum -- ^ <http://www.glfw.org/docs/3.1/group__errors.html#ga76f6bb9c4eea73db675f096b404593ce doc>
+  | Error'InvalidValue -- ^ <http://www.glfw.org/docs/3.1/group__errors.html#gaaf2ef9aa8202c2b82ac2d921e554c687 doc>
+  | Error'OutOfMemory -- ^ <http://www.glfw.org/docs/3.1/group__errors.html#ga9023953a2bcb98c2906afd071d21ee7f doc>
+  | Error'ApiUnavailable -- ^ <http://www.glfw.org/docs/3.1/group__errors.html#ga56882b290db23261cc6c053c40c2d08e doc>
+  | Error'VersionUnavailable -- ^ <http://www.glfw.org/docs/3.1/group__errors.html#gad16c5565b4a69f9c2a9ac2c0dbc89462 doc>
+  | Error'PlatformError -- ^ <http://www.glfw.org/docs/3.1/group__errors.html#gad44162d78100ea5e87cdd38426b8c7a1 doc>
+  | Error'FormatUnavailable -- ^ <http://www.glfw.org/docs/3.1/group__errors.html#ga196e125ef261d94184e2b55c05762f14 doc>
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
 --------------------------------------------------------------------------------
 -- Initialization and version information
 
+-- | The library version of the GLFW implementation in use.
+-- See <http://www.glfw.org/docs/3.1/intro.html#intro_version Version Management>
 data Version = Version
   { versionMajor    :: Int
   , versionMinor    :: Int
@@ -43,15 +48,19 @@ data Version = Version
 --------------------------------------------------------------------------------
 -- Monitor handling
 
+-- | Represents a physical monitor that's currently connected.
+-- See the <http://www.glfw.org/docs/3.1/monitor.html Monitor Guide>
 newtype Monitor = Monitor
   { unMonitor :: Ptr C'GLFWmonitor
   } deriving (Data, Eq, Ord, Show, Typeable, Generic)
 
+-- | Part of the 'MonitorCallback', for when a monitor gets connected or disconnected.
 data MonitorState =
     MonitorState'Connected
   | MonitorState'Disconnected
   deriving (Data, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | See <http://www.glfw.org/docs/3.1/monitor.html#monitor_modes Video Modes>
 data VideoMode = VideoMode
   { videoModeWidth       :: Int
   , videoModeHeight      :: Int
@@ -61,15 +70,17 @@ data VideoMode = VideoMode
   , videoModeRefreshRate :: Int
   } deriving (Data, Eq, Ord, Read, Show, Typeable, Generic)
 
--- It would be bad to give clients a way to construct invalid gamma ramps with
--- lists of unequal length, so this constructor should not be exported.
+-- | Lets you adjust the gamma of a monitor. To ensure that only valid values are created, use 'makeGammeRamp'.
+-- See <http://www.glfw.org/docs/3.1/monitor.html#monitor_gamma Gamma Ramp>.
 data GammaRamp = GammaRamp
+  -- NOTE: It would be bad to give clients a way to construct invalid gamma ramps
+  -- with lists of unequal length, so this constructor should not be exported.
   { gammaRampRed   :: [Int]
   , gammaRampGreen :: [Int]
   , gammaRampBlue  :: [Int]
   } deriving (Data, Eq, Ord, Read, Show, Typeable, Generic)
 
--- Smart constructor for GammaRamp.
+-- | Smart constructor for a GammaRamp.
 makeGammaRamp :: [Int] -> [Int] -> [Int] -> Maybe GammaRamp
 makeGammaRamp rs gs bs
     | lengthsEqual = Just $ GammaRamp rs gs bs
@@ -84,6 +95,7 @@ makeGammaRamp rs gs bs
 --------------------------------------------------------------------------------
 -- Window handling
 
+-- | Collects all the callbacks that can be associated with a Window into a single place.
 data WindowCallbacks = WindowCallbacks
   { storedCharFun             :: IORef C'GLFWcharfun
   , storedCursorEnterFun      :: IORef C'GLFWcursorenterfun
@@ -101,10 +113,15 @@ data WindowCallbacks = WindowCallbacks
   , storedDropFun             :: IORef C'GLFWdropfun
   }
 
+-- | Reprisents a GLFW window value.
+-- See the <http://www.glfw.org/docs/3.1/window.html Window Guide>
 newtype Window = Window
   { unWindow :: Ptr C'GLFWwindow
   } deriving (Data, Eq, Ord, Show, Typeable, Generic)
 
+-- | Lets you set various window hints before creating a 'Window'.
+-- See <http://www.glfw.org/docs/3.1/window.html#window_hints Window Hints>,
+-- particularly <http://www.glfw.org/docs/3.1/window.html#window_hints_values Supported and Default Values>.
 data WindowHint =
     WindowHint'Resizable           Bool
   | WindowHint'Visible             Bool
@@ -133,28 +150,33 @@ data WindowHint =
   | WindowHint'OpenGLProfile       OpenGLProfile
   deriving (Data, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | For use with the focus callback.
 data FocusState =
     FocusState'Focused
   | FocusState'Defocused
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | For use with the iconify callback. (note: iconified means minimized)
 data IconifyState =
     IconifyState'Iconified
   | IconifyState'NotIconified
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | The OpenGL robustness strategy.
 data ContextRobustness =
     ContextRobustness'NoRobustness
   | ContextRobustness'NoResetNotification
   | ContextRobustness'LoseContextOnReset
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | The OpenGL profile.
 data OpenGLProfile =
     OpenGLProfile'Any
   | OpenGLProfile'Compat
   | OpenGLProfile'Core
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | The type of OpenGL to create a context for.
 data ClientAPI =
     ClientAPI'OpenGL
   | ClientAPI'OpenGLES
@@ -163,6 +185,7 @@ data ClientAPI =
 --------------------------------------------------------------------------------
 -- Input handling
 
+-- | Part of the <http://www.glfw.org/docs/3.1/input.html#input_keyboard Keyboard Input> system.
 data Key =
     Key'Unknown
   | Key'Space
@@ -287,12 +310,14 @@ data Key =
   | Key'Menu
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | The state of an individual key when 'getKey' is called.
 data KeyState =
     KeyState'Pressed
   | KeyState'Released
   | KeyState'Repeating
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | For use with the <http://www.glfw.org/docs/3.1/input.html#joystick Joystick Input> system.
 data Joystick =
     Joystick'1
   | Joystick'2
@@ -312,11 +337,13 @@ data Joystick =
   | Joystick'16
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | If a given joystick button is pressed or not when 'getJoystickButtons' is called.
 data JoystickButtonState =
     JoystickButtonState'Pressed
   | JoystickButtonState'Released
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | Part of the <http://www.glfw.org/docs/3.1/input.html#input_mouse Mouse Input> system.
 data MouseButton =
     MouseButton'1
   | MouseButton'2
@@ -328,32 +355,44 @@ data MouseButton =
   | MouseButton'8
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | If the mouse button is pressed or not when 'getMouseButton' is called.
 data MouseButtonState =
     MouseButtonState'Pressed
   | MouseButtonState'Released
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | If the mouse's cursor is in the window or not.
 data CursorState =
     CursorState'InWindow
   | CursorState'NotInWindow
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | Allows for special forms of mouse input.
+-- See <http://www.glfw.org/docs/3.1/input.html#cursor_mode Cursor Modes>
 data CursorInputMode =
     CursorInputMode'Normal
   | CursorInputMode'Hidden
   | CursorInputMode'Disabled
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | When sticky keys is enabled, once a key is pressed it will remain pressed
+-- at least until the state is polled with 'getKey'. After that, if the key has
+-- been released it will switch back to released. This helps prevent problems
+-- with low-resolution polling missing key pressed. Note that use of the
+-- callbacks to avoid this problem the the recommended route, and this is just
+-- for a fallback.
 data StickyKeysInputMode =
     StickyKeysInputMode'Enabled
   | StickyKeysInputMode'Disabled
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | This is the mouse version of "StickyKeysInputMode".
 data StickyMouseButtonsInputMode =
     StickyMouseButtonsInputMode'Enabled
   | StickyMouseButtonsInputMode'Disabled
   deriving (Data, Enum, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | Modifier keys that were pressed as part of another keypress event.
 data ModifierKeys = ModifierKeys
   { modifierKeysShift   :: Bool
   , modifierKeysControl :: Bool
@@ -367,16 +406,21 @@ data ModifierKeys = ModifierKeys
 
 deriving instance Data CUChar
 
+-- | GLFW image data, for setting up custom mouse cursor appearnaces.
 data Image = Image
   { imageWidth  :: Int
   , imageHeight :: Int
   , imagePixels :: [CUChar]
   } deriving (Data, Eq, Ord, Read, Show, Typeable, Generic)
 
+-- | Reprisents a GLFW cursor.
 newtype Cursor = Cursor
   { unCursor :: Ptr C'GLFWcursor
   } deriving (Data, Eq, Ord, Show, Typeable, Generic)
 
+-- | Lets you use one of the standard cursor appearnaces that the local
+-- system theme provides for.
+-- See <http://www.glfw.org/docs/3.1/input.html#cursor_standard Standard Cursor Creation>.
 data StandardCursorShape =
     StandardCursorShape'Arrow
   | StandardCursorShape'IBeam

5f7aa61f4317a70d2c5d325667b70783c73bc3f9
Pavel Krajcevski <Krajcevski@gmail.com>
2018-03-24 10:25:44 -0700

    Don't actually need generalized newtype deriving

--- Graphics/UI/GLFW/Types.hs
+++ Graphics/UI/GLFW/Types.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
